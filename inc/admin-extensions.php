04cbca5eecbd863b30bb5ea07899c1130940cc1c
Zane M. Kolnik <zanematthew@gmail.com>
2012-11-30 16:42:27 -0500

    fixing extensions transient, #59

--- inc/admin-extensions.php
+++ inc/admin-extensions.php
@@ -4,15 +4,17 @@
  * Print and handle the data for the add-ons form
  */
 function sell_media_extensions_callback_fn(){
-    if ( false === ( $extensions = get_transient( 'graphpaperpress_extensions_feed' ) ) ) {
+
+    $extensions = get_transient( 'graphpaperpress_extensions_feed' );
+
+    if ( empty( $extensions ) ) {
         $response = wp_remote_get( 'http://graphpaperpress.com/json-extensions-feed/' );
         if ( ! is_wp_error( $response ) && isset( $response['body'] ) ) {
             $extensions = json_decode( $response['body'] );
             set_transient('graphpaperpress_extensions_feed', $extensions, 3600);
         }
     }
-
-    ?>
+?>
 <div class="wrap sell_media-extensions">
     <h2><?php _e( 'Extensions for Sell Media', 'sell_media' ); ?></h2>
     <p><?php _e( 'These extensions provide additonal functionality for the Sell Media plugin.', 'sell_media' ); ?></p>
