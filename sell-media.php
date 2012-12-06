1ff8d4fbd833dccef21f84a405cb1cf865b6b404
Zane M. Kolnik <zanematthew@gmail.com>
2012-12-06 17:34:21 -0500

    Updating tag.
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- sell-media.php
+++ sell-media.php
@@ -4,14 +4,14 @@
 Plugin Name: Sell Media
 Plugin URI: http://graphpaperpress.com/plugins/sell-media
 Description: A plugin for selling digital downloads and reprints.
-Version: 1.0.5
+Version: 1.0.6
 Author: Graph Paper Press
 Author URI: http://graphpaperpress.com
 Author Email: support@graphpaperpress.com
 License: GPL
 */
 
-define( 'SELL_MEDIA_VERSION', '1.0.5' );
+define( 'SELL_MEDIA_VERSION', '1.0.6' );
 define( 'SELL_MEDIA_PLUGIN_FILE', plugin_dir_path(__FILE__) . 'sell-media.php' );
 
 include( dirname(__FILE__) . '/inc/cart.php' );
