3e913f63d1d3c943101b298809a6193576c3392c
Zane M. Kolnik <zanematthew@gmail.com>
2013-04-02 15:34:56 -0400

    Adding feature back in to send users to the check out page.
    Signed-off-by: Zane M. Kolnik <zanematthew@gmail.com>

--- js/sell_media.js
+++ js/sell_media.js
@@ -194,7 +194,7 @@ jQuery( document ).ready(function( $ ){
     });
 
 
-    $( document ).on('submit', '#sell_media_cart_form', function(){
+    $( document ).one('submit', '#sell_media_cart_form', function(){
 
         var _data = "action=sell_media_add_items&taxonomy=licenses&" + $( this ).serialize();
 
@@ -214,9 +214,9 @@ jQuery( document ).ready(function( $ ){
         return false;
     });
 
-    // $( document ).on( 'click', '.sell-media-purchased', function(){
-    //     location.href = checkouturl;
-    // });
+    $( document ).on( 'click', '.sell-media-purchased', function(){
+        location.href = checkouturl;
+    });
 
     $( document ).on('click', '.remove-item-handle', function(){
 
