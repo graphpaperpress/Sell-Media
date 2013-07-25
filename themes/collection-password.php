01e34450263addfde7ac76bf025a42f136637a32
Zane M. Kolnik <zanematthew@gmail.com>
2013-07-25 09:44:27 -0400

    Forgot to add the new file.

---
+++ themes/collection-password.php
@@ -0,0 +1,6 @@
+<form action="" method="POST">
+     <p><?php _e( 'This collection is password protected','sell_media'); ?>
+     <input type="text" value="" name="collection_password" />
+     <input type="submit" value="<?php _e( 'Submit', 'sell_media' ); ?>" name="submit" />
+     </p>
+</form>
