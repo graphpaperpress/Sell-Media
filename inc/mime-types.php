2b41221a680168c7812262eb59739aa83628e650
Thad Allender <thadallender@gmail.com>
2014-03-20 18:18:36 -0400

    mime-types added

---
+++ inc/mime-types.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Mime Types
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Allowed Mime Types
+ *
+ * @since 1.0
+ *
+ * @param array $existing_mimes A list of all the existing MIME types
+ * @return array A list of all the new MIME types appended
+ */
+function sell_media_allowed_mime_types( $existing_mimes = array() ) {
+	$existing_mimes['zip']  = 'application/zip';
+	$existing_mimes['epub'] = 'application/epub+zip';
+	$existing_mimes['mobi'] = 'application/x-mobipocket-ebook';
+	$existing_mimes['m4r']  = 'audio/aac';
+	$existing_mimes['aif']  = 'audio/x-aiff';
+	$existing_mimes['aiff'] = 'audio/aiff';
+	$existing_mimes['psd']  = 'image/photoshop';
+	$existing_mimes['apk']  = 'application/vnd.android.package-archive';
+	$existing_mimes['msi']  = 'application/x-ole-storage';
+
+	return $existing_mimes;
+}
+add_filter( 'upload_mimes', 'sell_media_allowed_mime_types' );
