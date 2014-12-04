399e855654f82687a2030257233b4ef6270d4734
Thad Allender <thadallender@gmail.com>
2014-12-04 15:35:04 -0500

    version 1.9.12

--- sell-media.php
+++ sell-media.php
@@ -25,7 +25,7 @@
  * @package SellMedia
  * @category Core
  * @author Thad Allender
- * @version 1.9.11
+ * @version 1.9.12
  */
 
 // Exit if accessed directly
@@ -161,7 +161,7 @@ final class SellMedia {
 
         // Plugin version
         if ( ! defined( 'SELL_MEDIA_VERSION' ) ) {
-            define( 'SELL_MEDIA_VERSION', '1.9.11' );
+            define( 'SELL_MEDIA_VERSION', '1.9.12' );
         }
 
         // Plugin Folder Path
