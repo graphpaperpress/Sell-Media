eb1dacd8faa6973dbe7455ca273ce44bd2098c5c
Mikhail Bylov <michaelb@Mikhails-Mac-mini.local>
2015-04-28 16:26:24 -0400

    fix:  moves non image files to /uploads/sell_media/

--- inc/admin-helpers.php
+++ inc/admin-helpers.php
@@ -39,18 +39,17 @@ function sell_media_move_file( $attachment_id ){
 function sell_media_default_move( $original_file=null ){
 
     $dir = wp_upload_dir();
-    $original_file_path = $dir['basedir'] . '/' . $original_file;
-    $destination_file_path = sell_media_get_upload_dir() . '/' . $original_file;
+    $destination_file_path = str_replace( $dir['basedir'], sell_media_get_upload_dir(), $original_file );
 
-    if ( file_exists( $original_file_path ) ){
+    if ( file_exists( $original_file ) ){
 
         if ( ! file_exists( dirname( $destination_file_path ) ) ){
             wp_mkdir_p( dirname( $destination_file_path ) );
         }
 
         // Copy original to our protected area
-        @copy( $original_file_path, $destination_file_path );
-        @unlink( $original_file_path );
+        @copy( $original_file, $destination_file_path );
+        @unlink( $original_file );
     }
 }
 
