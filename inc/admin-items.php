d27f44272273060e02fb013fa1b721589b27713e
Thad Allender <thadallender@gmail.com>
2015-05-01 14:58:47 -0400

    added server path info

--- inc/admin-items.php
+++ inc/admin-items.php
@@ -61,7 +61,8 @@ function sell_media_files_meta_box( $post ) {
 
     <div id="sell-media-upload-show-options" class="sell-media-upload-show-options" style="display:none;">
         <h4><?php _e( 'Importing', 'sell_media' ); ?></h4>
-        <p class="description"><?php printf( __( 'Quickly import folders of images using this option. Use FTP or <a href="%1$s" target="_blank">export directly from Lightroom</a> and place new folders into the wp-content/uploads/sell_media/import/ folder. Then, select the folder below to import into WordPress.', 'sell_media' ), 'http://graphpaperpress.com/docs/sell-media/#add-bulk' ); ?></p>
+        <p class="description"><?php printf( __( 'Quickly import folders of images using this option. Use FTP or <a href="%1$s" target="_blank">export directly from Lightroom</a> and place new folders into the server path listed below. Then, select the folder below to import into WordPress.', 'sell_media' ), 'http://graphpaperpress.com/docs/sell-media/#add-bulk' ); ?></p>
+        <p class="description"><strong>Server Path:</strong> <?php echo sell_media_get_import_dir(); ?></p>
         <select id="sell-media-upload-bulk-selector" value="">
             <option value=""><?php _e( 'Select a folder', 'sell_media' ); ?></option>
             <?php
