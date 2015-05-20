ec9314c65f826157782038aabdd39cf65c2ee0f7
fxbenard <fxbenard@gmail.com>
2015-05-20 22:34:00 +0200

    Remove unused replace grunt task

--- grunt/replace.js
+++ grunt/replace.js
@@ -6,25 +6,5 @@ module.exports = {
           from: /Stable tag: (.*)/,
           to: "Stable tag: <%= pkg.version %>"
         }]
-      },
-      main_php: {
-        src: [ '<%= pkg.pot.src %>' ],
-        overwrite: true,
-        replacements: [{
-          from: /define(.*)_VER'.*/,
-          to: "define( '<%= pkg.constant.ver %>' , '<%= pkg.version %>' );"
-        },{
-          from: / Version:\s*(.*)/,
-          to: " Version: <%= pkg.version %>"
-        },{
-          from: /EDD_(.*)_VER/,
-          to: "<%= pkg.constant.ver %>"
-        },{
-           from: /EDD_(.*)_DIR/,
-          to: "<%= pkg.constant.dir %>"
-        },{
-            from: /EDD_(.*)_URL/,
-          to: "<%= pkg.constant.url %>"
-        }]
       }
     };
