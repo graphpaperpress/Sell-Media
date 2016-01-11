4569decaaf9d6618b5e3a29b9ab47912e792a5a2
Thad Allender <thadallender@gmail.com>
2016-01-11 16:41:38 -0500

    remove cached grunt files

--- grunt/phpcs.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    application: {
-        src: ['*.php', '!node_modules/**', '!build/**',]
-        },
-        options: {
-            bin: 'phpcs',
-            standard: 'WordPress',
-        }
-};
--- grunt/wp_readme_to_markdown.js
+++
@@ -1,8 +0,0 @@
-// https://www.npmjs.com/package/grunt-wp-readme-to-markdown
-module.exports = {
-    your_target: {
-        files: {
-            'README.md': 'readme.txt'
-        },
-    },
-};
