80adc87489a9afb5284998ae17b254666320cff1
Rakesh Lawaju <codearryaas@gmail.com>
2016-01-26 16:27:33 +0545

    Paypal request class included

--- sell-media.php
+++ sell-media.php
@@ -228,6 +228,7 @@ if ( ! class_exists( 'SellMedia' ) ) :
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/deprecated.php';
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/helpers.php';
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/gateways/paypal.php';
+			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/gateways/class-sm-gateway-paypal-request.php';
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/mime-types.php';
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/scripts.php';
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/shortcodes.php';
