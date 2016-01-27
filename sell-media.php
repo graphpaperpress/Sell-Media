1af0d732f48d6b106a40509018cd933f30bda513
Rakesh Lawaju <codearryaas@gmail.com>
2016-01-27 09:14:32 +0545

    Fixed instance issue

--- sell-media.php
+++ sell-media.php
@@ -122,13 +122,13 @@ if ( ! class_exists( 'SellMedia' ) ) :
 				self::$instance->products       = new SellMediaProducts();
 				self::$instance->search         = new SellMediaSearch();
 
-				if ( self::is_request( 'admin' ) ) {
+				if ( self::$instance->is_request( 'admin' ) ) {
 					self::$instance->notices        = new SellMediaAdminNotices();
 					self::$instance->admin_search   = new SellMediaAdminSearch();
 				}
 
 				// Set cart global variable.
-				if( self::is_request( 'frontend' ) ){
+				if( self::$instance->is_request( 'frontend' ) ){
 					$GLOBALS['sm_cart'] = new SellMediaCart();
 				}
 			}
@@ -240,7 +240,7 @@ if ( ! class_exists( 'SellMedia' ) ) :
 			require_once SELL_MEDIA_PLUGIN_DIR . '/inc/lightbox.php';
 
 			// Load files if is front end.
-			if( $this->is_request( 'frontend' ) ){
+			if( self::$instance->is_request( 'frontend' ) ){
 
 				require_once SELL_MEDIA_PLUGIN_DIR . '/inc/class-phpsessions.php';
 				require_once SELL_MEDIA_PLUGIN_DIR . '/inc/class-cart.php';
@@ -248,7 +248,7 @@ if ( ! class_exists( 'SellMedia' ) ) :
 
 			}
 
-			if ( $this->is_request( 'admin' ) ) {
+			if ( self::$instance->is_request( 'admin' ) ) {
 
 				require_once SELL_MEDIA_PLUGIN_DIR . '/inc/admin-helpers.php';
 				require_once SELL_MEDIA_PLUGIN_DIR . '/inc/admin-items.php';
