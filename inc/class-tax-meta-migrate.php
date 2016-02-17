7d75f4aadd66b5d7644ffabbe4b6298cda4711c5
Rakesh Lawaju <codearryaas@gmail.com>
2016-02-17 17:14:15 +0545

    Fixed the tax migration for multisite

--- inc/class-tax-meta-migrate.php
+++ inc/class-tax-meta-migrate.php
@@ -28,21 +28,52 @@ Class SellMediaTaxMetaMigrate {
         if ( $version && $version > SELL_MEDIA_VERSION )
             return;
 
-        $metas = $this->get_all_meta();
+        if( is_multisite() ){
+            $sites = wp_get_sites();
+            if( !empty( $sites ) ){
+                foreach ( $sites as $key => $site ) {
+                    $table_prefix = $this->table_prefix( absint( $site['blog_id'] ) );
+                    $migrate = $this->migrate( $table_prefix );
+                }
+            }
+        }
+        else{
+            $table_prefix = $this->table_prefix();
+            $migrate = $this->migrate( $table_prefix );
+        }
+    }
+
+    private function migrate( $table_prefix ){
+        if( '' === $table_prefix )
+            return false;
+        global $wpdb;
+        $metas = $this->get_all_meta( $table_prefix );
         $drop_table = true;
 
         if( !empty( $metas ) ){
             foreach ($metas as $key => $meta) {
-                $result = update_term_meta ( (int) $meta->taxonomy_id, $meta->meta_key, $meta->meta_value );
+                $result = $wpdb->insert( 
+                                $table_prefix . 'termmeta', 
+                                array( 
+                                    'term_id' => $meta->taxonomy_id, 
+                                    'meta_key' => $meta->meta_key,
+                                    'meta_value' => $meta->meta_value
+                                ), 
+                                array( 
+                                    '%d', 
+                                    '%s',
+                                    '%s' 
+                                ) 
+                            );
 
-                if( is_wp_error( $result ) || false === $result ){
+                if( false === $result ){
                     $drop_table = false;
                 }
             }
         }
 
         if( $drop_table ){
-            $this->delete_meta_table();
+            $this->delete_meta_table( $table_prefix );
         }
     }
 
@@ -50,9 +81,9 @@ Class SellMediaTaxMetaMigrate {
      * Get old tax metas.
      * @return object Old tax metas
      */
-    private function get_all_meta(){
+    private function get_all_meta( $table_prefix ){
         global $wpdb;
-        $sql = 'SELECT * FROM `'.$wpdb->prefix.'taxonomymeta`';
+        $sql = 'SELECT * FROM `'.$table_prefix.'taxonomymeta`';
         $metas = $wpdb->get_results( $sql );
         return $metas;
     }
@@ -61,10 +92,22 @@ Class SellMediaTaxMetaMigrate {
      * Delete old meta table.
      * @return mixed Query output.
      */
-    private function delete_meta_table(){
+    private function delete_meta_table( $table_prefix ){
         global $wpdb;
-        $sql = 'DROP TABLE `'.$wpdb->prefix.'taxonomymeta`';
+        $sql = 'DROP TABLE `'.$table_prefix.'taxonomymeta`';
         return $wpdb->query( $sql );
     }
 
+    private function table_prefix( $blog_id = NULL ){
+        global $wpdb;
+        $prefix =  $wpdb->prefix;
+        $base_prefix =  $wpdb->base_prefix;
+
+        if( !is_multisite() || is_null( $blog_id ) ||  1 === $blog_id ){
+            return $prefix;
+        }
+
+        $prefix .= $blog_id . '_';
+        return $prefix;
+    }
 }
