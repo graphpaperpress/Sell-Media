9ec4ddccbcd2e2787688535c08e8ba52720fcc6d
Rakesh Lawaju <codearryaas@gmail.com>
2016-02-17 12:46:09 +0545

    Keeping old tax function so to prevent from errors.

--- inc/term-meta.php
+++ inc/term-meta.php
@@ -586,4 +586,69 @@ class SellMediaTaxonomyMetadata {
             $this->setup_blog($blog_id);
     }
 }
+
+//
+// Taxonomy meta Deprecated functions. 
+// Keeping old function so to prevent from errors. 
+//
+
+/**
+ * Add meta data field to a term.
+ *
+ * @param int $term_id Post ID.
+ * @param string $key Metadata name.
+ * @param mixed $value Metadata value.
+ * @param bool $unique Optional, default is false. Whether the same key should not be added.
+ * @return bool False for failure. True for success.
+ */
+function sell_media_add_term_meta($term_id, $meta_key, $meta_value, $unique = false) {
+    return add_term_meta( $term_id, $meta_key, $meta_value, $unique );
+}
+
+/**
+ * Remove metadata matching criteria from a term.
+ *
+ * You can match based on the key, or key and value. Removing based on key and
+ * value, will keep from removing duplicate metadata with the same key. It also
+ * allows removing all metadata matching key, if needed.
+ *
+ * @param int $term_id term ID
+ * @param string $meta_key Metadata name.
+ * @param mixed $meta_value Optional. Metadata value.
+ * @return bool False for failure. True for success.
+ */
+function sell_media_delete_term_meta($term_id, $meta_key, $meta_value = '') {
+    return delete_term_meta( $term_id, $meta_key, $meta_value );
+}
+
+/**
+ * Retrieve term meta field for a term.
+ *
+ * @param int $term_id Term ID.
+ * @param string $key The meta key to retrieve.
+ * @param bool $single Whether to return a single value.
+ * @return mixed Will be an array if $single is false. Will be value of meta data field if $single
+ *  is true.
+ */
+function sell_media_get_term_meta($term_id, $key, $single = false) {
+    return get_term_meta( $term_id, $key, $single );
+}
+
+/**
+ * Update term meta field based on term ID.
+ *
+ * Use the $prev_value parameter to differentiate between meta fields with the
+ * same key and term ID.
+ *
+ * If the meta field for the term does not exist, it will be added.
+ *
+ * @param int $term_id Term ID.
+ * @param string $key Metadata key.
+ * @param mixed $value Metadata value.
+ * @param mixed $prev_value Optional. Previous value to check before removing.
+ * @return bool False on failure, true if success.
+ */
+function sell_media_update_term_meta($term_id, $meta_key, $meta_value, $prev_value = '') {
+    return update_term_meta( $term_id, $meta_key, $meta_value, $prev_value );
+}
 // End 'taxonomy meta plugin code'
