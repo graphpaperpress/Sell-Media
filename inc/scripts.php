7ec694ccd0c2403bce87a1807bfbb138b02fe637
Racase lawaju <codearryaas@gmail.com>
2016-04-22 15:39:30 +0545

    Localize tax per item value

--- inc/scripts.php
+++ inc/scripts.php
@@ -62,6 +62,7 @@ function sell_media_scripts( $hook ) {
             ),
         'cart_style' => apply_filters( 'sell_media_cart_style', 'table' ),
         'tax' => ( empty( $settings->tax ) ) ? 0 : $settings->tax_rate,
+        'tax_per_item' => ( isset( $settings->per_item_tax ) && !empty( $settings->per_item_tax ) ) ? true : false,
         'shipping' => apply_filters( 'sell_media_shipping', 0 ), // should PayPal force buyers add address
         'cart_error' => __( 'There was an error loading the cart data. Please contact the site owner.', 'sell_media' ),
         'checkout_text' => __( 'Checkout Now', 'sell_media' ),
