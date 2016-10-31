a716121e92f13752fb0c97f9cbb2b88d933095ad
Thad Allender <thadallender@gmail.com>
2016-10-30 23:15:32 -0400

    margins on sell-media-item

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -8,23 +8,29 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Deprecated functions
  */
-function sell_media_item_has_taxonomy_terms( $post_id=null, $taxonomy=null ) {
-    return false;
+function sell_media_item_has_taxonomy_terms( $post_id = null, $taxonomy = null ) {
+	return false;
 }
-function sell_media_country_list( $current=null, $req=false ){
-    return false;
+
+function sell_media_country_list( $current = null, $req = false ) {
+	return false;
 }
-function sell_media_countries_list(){
-    return false;
+
+function sell_media_countries_list() {
+	return false;
 }
-function sell_media_us_states_list( $current=null, $req=false ){
-    return false;
+
+function sell_media_us_states_list( $current = null, $req = false ) {
+	return false;
 }
-function sell_media_collections(){
-    sell_media_get_taxonomy_terms( 'collection' );
+
+function sell_media_collections() {
+	sell_media_get_taxonomy_terms( 'collection' );
 }
