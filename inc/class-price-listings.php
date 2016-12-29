2a69065708a4749dfeb92a7b6d4b10b9163e99bb
Rakesh Lawaju <codearryaas@gmail.com>
2016-12-29 14:27:55 +0545

    Pass args in action

--- inc/class-price-listings.php
+++ inc/class-price-listings.php
@@ -103,7 +103,7 @@ class Sell_Media_Price_Listings {
 			$url = add_query_arg( $url_parameters, $url );
 			?>
 			<div id="poststuff">
-				<?php do_action( 'sell_media_pricelists_before_form' ); ?>
+				<?php do_action( 'sell_media_pricelists_before_form', $this->current_tab, $url ); ?>
 				<form method="post" action="<?php echo esc_url( $url ); ?>" id="sell-media-pricelist-form">
 					<?php
 					wp_nonce_field( 'sell-media-price-list-page' );
@@ -114,7 +114,7 @@ class Sell_Media_Price_Listings {
 					}
 					?>
 				</form>
-				<?php do_action( 'sell_media_pricelists_after_form' ); ?>
+				<?php do_action( 'sell_media_pricelists_after_form', $this->current_tab, $url ); ?>
 			</div>
 		</div>
 	<?php
