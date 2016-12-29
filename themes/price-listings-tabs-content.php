d117ea0bcc33102947d23201d2bf8b949026015e
Rakesh Lawaju <codearryaas@gmail.com>
2016-12-29 14:22:24 +0545

    Remove pricelist title section

--- themes/price-listings-tabs-content.php
+++ themes/price-listings-tabs-content.php
@@ -1,17 +1,8 @@
 <?php
-$tab = $this->tab;
 $current_term = get_term( (int) $this->current_term, $current_tab );
 $download_parents = $this->get_terms();
 $url = home_url( add_query_arg( array( 'term_parent' => 'new' ) ) );
 ?>
-<h2 class="tab-title">
-	<span><?php echo __( 'Pricelists for ', 'sell_media' ) . ' ' . $tab['tab_title']; ?></span>
-	<a class="page-title-action tab-create-new-list" href="javascript:void(0);"><?php _e( 'Add New Pricelist', 'sell_media' ); ?> </a>
-	<div class="sell-media-add-new-pricelist-popup">
-		<input type="text" name="new_term_name" />
-		<input type="submit" name="Submit" class="button-primary" value="<?php _e( 'Add', 'sell_media' ); ?>" />
-	</div>
-</h2>
 <div class="tab-price-lists">
 	<select>
 		<?php
