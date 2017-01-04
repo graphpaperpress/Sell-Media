a73cfe2593d695d54e61645f6543022d9d029d7c
Thad Allender <thadallender@gmail.com>
2017-01-04 12:29:30 -0500

    settings in tabs

--- themes/admin-add-item-main-container.php
+++ themes/admin-add-item-main-container.php
@@ -11,15 +11,15 @@
 		<li><a href="#sell-media-file-upload" class="sell-media-tab-nav-active"><?php esc_html_e( 'File Upload', 'sell_media' ); ?></a></li>
 		<li><a href="#sell-media-settings"><?php esc_html_e( 'Settings', 'sell_media' ); ?></a></li>
 		<li><a href="#sell-media-stats"><?php esc_html_e( 'Stats', 'sell_media' ); ?></a></li>
-		<li><a href="#sell-media-seo"><?php esc_html_e( 'Seo', 'sell_media' ); ?></a></li>
-		<li><a href="#sell-media-advance"><?php esc_html_e( 'Advance Options', 'sell_media' ); ?></a></li>
+		<li><a href="#sell-media-seo"><?php esc_html_e( 'SEO', 'sell_media' ); ?></a></li>
+		<li><a href="#sell-media-advanced"><?php esc_html_e( 'Advanced Options', 'sell_media' ); ?></a></li>
 	</ul>
 	<div class="main-container-tabs-contents">
 		<div id="sell-media-file-upload" class="ui-state-active sell-media-tab-content">
 			<?php echo sell_media_files_meta_box( $post ); ?>
 		</div>
 		<div id="sell-media-settings" class="sell-media-tab-content">
-			#tab2
+			<?php echo sell_media_options_meta_box( $post ); ?>
 		</div>
 		<div id="sell-media-stats" class="sell-media-tab-content">
 			<?php echo sell_media_stats_meta_box( $post ); ?>
@@ -27,8 +27,8 @@
 		<div id="sell-media-seo" class="sell-media-tab-content">
 			<?php sell_media_editor(); ?>
 		</div>
-		<div id="sell-media-advance" class="sell-media-tab-content">
-			#tab5
+		<div id="sell-media-advanced" class="sell-media-tab-content">
+			<?php echo sell_media_options_meta_box( $post ); ?>
 		</div>
 	</div>
 </div>
