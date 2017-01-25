acb51e08fc3476cd185bd9dc7b4da0f11edbe0c6
Rakesh Lawaju <codearryaas@gmail.com>
2017-01-25 12:27:30 +0545

    Added action hook before and after tab callback

--- themes/admin-add-item-main-container.php
+++ themes/admin-add-item-main-container.php
@@ -28,7 +28,10 @@ if ( empty( $tabs ) ) {
 		<div id="sell-media-<?php echo esc_attr( $key ); ?>" class="ui-state-active sell-media-tab-content">
 			<h3 class="sell-media-tab-content-title"><?php echo esc_attr( $tab['content_title'] ); ?></h3>
 			<?php
-			call_user_func( $tab['content_callback'], $post ); ?>
+			do_action( 'sell_media_add_item_tab_before_content_callback', $key, $post );
+			call_user_func( $tab['content_callback'], $post );
+			do_action( 'sell_media_add_item_tab_after_content_callback', $key, $post );
+			?>
 		</div>
 		<?php endforeach; ?>
 	</div>
