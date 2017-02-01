6c635e26900d1ad8e55fd1c9eb101a14af46aa9a
Rakesh Lawaju <codearryaas@gmail.com>
2017-02-01 22:46:00 +0545

    Added SellMediaNavStyleUI class in deprecated list

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -36,11 +36,79 @@ function sell_media_collections() {
 }
 
 /**
- * Sell Media content loop. 
+ * Sell Media content loop.
  *
  * @deprecated 2.2.6 Use sell_media_content_loop filter.
  */
 function sell_media_content_loop( $post_id, $index ) {
 	_deprecated_function( __FUNCTION__, '2.2.6', 'sell_media_content_loop filter' );
 	return apply_filters( 'sell_media_content_loop', $post_id, $index );
+}
+
+/**
+ * Sell Media nav style UI.
+ *
+ * @deprecated 2.3 Use Sell_Media_Price_Listings.
+ */
+class SellMediaNavStyleUI {
+
+	/**
+	 * Constructor.
+	 */
+	function __construct() {
+		return false;
+	}
+
+	/**
+	 * Magic method.
+	 *
+	 * @param string $name  name.
+	 * @param string $value Value.
+	 */
+	function __set( $name, $value ) {
+		return false;
+	}
+
+	/**
+	 * Magic methods.
+	 *
+	 * @param  string $name Name.
+	 */
+	function __get( $name ) {
+		return false;
+	}
+
+	/**
+	 * Admin scripts.
+	 */
+	function admin_scripts() {
+		return false;
+	}
+
+	/**
+	 * Save term.
+	 */
+	function save_term() {
+		return false;
+	}
+
+	/**
+	 * Delete term.
+	 */
+	function delete_term() {
+		return false;
+	}
+
+	/**
+	 * Add term.
+	 */
+	function add_term() {
+		return false;
+	}
+	/**
+	 * Setting UI.
+	 */
+	function setting_ui() {
+		return false;
+	}
 }
