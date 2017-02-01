7860daa60f5517cd60592124241018da4cfd1b8a
Rakesh Lawaju <codearryaas@gmail.com>
2017-02-01 14:49:03 +0545

    Added delete link

--- themes/price-listings-tabs-content.php
+++ themes/price-listings-tabs-content.php
@@ -2,18 +2,26 @@
 $current_term = get_term( (int) $this->current_term, $current_tab );
 $download_parents = $this->get_terms();
 $url = add_query_arg( array( 'term_parent' => 'new' ), admin_url( 'edit.php?' . $_SERVER['QUERY_STRING'] ) );
+$current_url = false;
 ?>
 <div class="tab-price-lists">
 	<select>
 		<?php
 		foreach ( $download_parents as $slug => $term ) {
 			$url = add_query_arg( array( 'term_parent' => $term->term_id ), admin_url( 'edit.php?' . $_SERVER['QUERY_STRING'] ) );
+			if ( $this->current_term === $term->term_id ) {
+				$current_url = $url;
+			}
 			echo "<option value='$url' " . selected( (int) $this->current_term, $term->term_id, false ) . ">" . $term->name . '</option>';
 		}
 		?>
 	</select>
 	<input type="hidden" value="<?php echo !empty( $current_term ) ? $current_term->term_id: 'new'; ?>" name="term_id" />
 	<input type="hidden" value="" name="deleted_term_ids" />
+	<?php
+	$delete_url = add_query_arg( $current_url, array( 'delete' => true ) );
+	$delete_url = wp_nonce_url( $delete_url, 'delete_pricelist_nonce_action', 'delete_pricelist_nonce_name' ); ?>
+	<a href="#" data-href="<?php echo esc_url( $delete_url ); ?>" class="deletion" title="<?php esc_html_e( 'Delete pricelist.', 'sell_media' ); ?>" data-message="<?php printf( __( 'Are you sure you want to delete the pricelist: %s', 'sell_media' ), $current_term->name ); ?>"><?php esc_html_e( 'Delete', 'sell_media' ); ?></a>
 </div>
 <hr/>
 <?php
