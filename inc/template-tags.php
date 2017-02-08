6feb250367bf33de93db672a634c9159d623f781
Rakesh Lawaju <codearryaas@gmail.com>
2017-02-08 12:17:07 +0545

    Add filter to enable/ disable ecommerce feature

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -227,7 +227,11 @@ function sell_media_gallery( $post_id ) {
 		$html .= '<div id="sell-media-' . $attachment_id . '" class="' . $item_class . ' sell-media-grid-single-item">';
 		$html .= '<a href="' . esc_url( get_permalink( $attachment_id ) ) . '" ' . sell_media_link_attributes( $attachment_id ) . ' class="sell-media-item">';
 		$html .= wp_get_attachment_image( $attachment_id, apply_filters( 'sell_media_thumbnail', 'medium' ), '', $attr );
-		$html .= '<div class="sell-media-quick-view" data-product-id="' . esc_attr( $post_id ) . '" data-attachment-id="' . esc_attr( $attachment_id ) . '">' . apply_filters( 'sell_media_quick_view_text', __( 'Quick View', 'sell_media' ), $post_id, $attachment_id ) . '</div>';
+
+		$enable_ecommerce = apply_filters( 'sell_media_enable_ecommerce', true );
+		if ( $enable_ecommerce ) {
+			$html .= '<div class="sell-media-quick-view" data-product-id="' . esc_attr( $post_id ) . '" data-attachment-id="' . esc_attr( $attachment_id ) . '">' . apply_filters( 'sell_media_quick_view_text', __( 'Quick View', 'sell_media' ), $post_id, $attachment_id ) . '</div>';
+		}
 		$html .= '</a>';
 		$html .= '</div>';
 	}
