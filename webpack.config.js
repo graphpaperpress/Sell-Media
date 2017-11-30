a830b815e6d756f6a602966bde38a4f8cd73419c
Thad Allender <thadallender@gmail.com>
2017-11-30 10:22:48 -0500

    fix image paths

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: 'images/[name].[ext]?[hash]'
+          name: 'images/[name].[ext]?[hash]',
+          publicPath: '../',
         }
       }
     ],
