4c3e3c1ac34cbe4c4d8a51dc23c786f9f3d58a05
Thad Allender <thadallender@gmail.com>
2017-11-29 22:23:39 -0500

    image filepaths

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name].[ext]?[hash]',
-          outputPath: 'images/',
+          publicPath: '../images/',
         }
       }
     ],
