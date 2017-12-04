f23a05f0e76d31538db4e367c4a679d0249e50d8
Thad Allender <thadallender@gmail.com>
2017-12-04 10:56:03 -0500

    added Post.vue

---
+++ src/components/Post.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="column is-mobile is-one-third">
+		<a v-bind:href="p.link" v-bind:title="p.title.rendered">
+			<img v-bind:src="p.media_details.sizes.medium_large.source_url">
+			<h2>{{ p.title.rendered }}</h2>
+		</a>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+
+		props: ['p'],
+
+    }
+
+</script>
