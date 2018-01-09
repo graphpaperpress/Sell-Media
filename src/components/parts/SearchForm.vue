abf9b144b0ec3f10c8763bcf96742532a2e75b5e
Thad Allender <thadallender@gmail.com>
2018-01-09 09:27:52 -0500

    searchform fixes

--- src/components/parts/SearchForm.vue
+++ src/components/parts/SearchForm.vue
@@ -4,13 +4,14 @@
 		<div class="field has-addons">
 			<p class="control">
 				<span class="select">
-					<select>
-						<option v-for="type in types" v-if="type.count > 0">{{ type.name }}</option>
+					<select v-model="search_type">
+						<option value=""></option>
+						<option v-for="type in types" v-if="type.count > 0" :value="type.slug">{{ type.name }}</option>
 					</select>
 				</span>
 			</p>
 			<p class="control">
-				<input v-model="search" class="input" type="text" :placeholder="labels.search">
+				<input v-model="search" class="input" type="text" :placeholder="labels.search" @keyup.enter="$emit('search', search)">
 			</p>
 			<p class="control">
 				<button class="button is-info" @click="$emit('search', search)">
@@ -31,7 +32,9 @@
 		data: function() {
 			return {
 				types: {},
-				labels: sell_media.search_labels
+				labels: sell_media.search_labels,
+				search_type: '',
+				search: ''
 			}
 		},
 
