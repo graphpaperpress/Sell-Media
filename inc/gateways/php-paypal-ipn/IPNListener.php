b55042d8b0c43b61f0dd37d4f4801794b6657d4e
prazna <maharzan@github.com>
2020-03-11 18:53:47 +0545

    Fixing PayPal Certificate so checkout and payments are working

--- inc/gateways/php-paypal-ipn/IPNListener.php
+++ inc/gateways/php-paypal-ipn/IPNListener.php
@@ -99,7 +99,7 @@ class IPNListener
 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/api_cert_chain.crt');
+		curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cert/cacert.pem');
         curl_setopt($ch, CURLOPT_URL, $uri);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $encoded_data);
