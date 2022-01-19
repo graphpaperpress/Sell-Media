c45563d5a6c61c455d6bb22a59ee21ab23f87a77
prazna <maharzan@github.com>
2022-01-19 14:52:37 +0545

    fixing notice on widgets page

--- gutenberg/build/index.asset.php
+++ gutenberg/build/index.asset.php
@@ -1,1 +1,10 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-editor', 'wp-element'), 'version' => '6c63fc0260e6a67764a3652eff7a925f');
+<?php 
+
+// check which page is loaded
+global $pagenow;
+
+if ( 'widgets.php' == $pagenow ) {
+	return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-edit-widgets', 'wp-element'), 'version' => '6c63fc0260e6a67764a3652eff7a928f');
+} else {
+	return array('dependencies' => array('wp-blocks', 'wp-components', 'wp-editor', 'wp-element'), 'version' => '6c63fc0260e6a67764a3652eff7a925f');
+}
